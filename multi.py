17693942ae3a0e8182179eb9ae560eadb068d14b
zidan_1013 <80787878+zidan4@users.noreply.github.com>
2025-01-05 18:49:09 +0300

    Update multi.py

--- multi.py
+++ multi.py
@@ -13,3 +13,19 @@ regr.fit(X, y)
 predictedCO2 = regr.predict([[2300, 1300]])
 
 print(predictedCO2) 
+
+#my prefered way
+import pandas
+from sklearn import linear_model
+
+df = pandas.read_csv("data.csv")
+
+X = df[['Weight', 'Volume']]
+y = df['CO2']
+
+regr = linear_model.LinearRegression()
+regr.fit(X, y)
+
+predictedCO2 = regr.predict([[3300, 1300]])
+
+print(predictedCO2) 
